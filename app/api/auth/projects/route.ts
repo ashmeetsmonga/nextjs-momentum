498e258c21d669fe5122d529c7a08bc7c62168e2
ashmeetsmonga <ashmeetsmonga@gmail.com>
2025-01-20 20:29:12 +0530

    feat: Add projects get api

---
+++ app/api/auth/projects/route.ts
@@ -0,0 +1,13 @@
+import prisma from "@/app/libs/prisma";
+import { errorHandler } from "@/app/utils/errorHandler";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, res: Response) {
+  try {
+    const userId = "67756d4ef0e0eded7ddb2171";
+    const projects = await prisma.project.findMany({ where: { ownerId: userId } });
+    return NextResponse.json({ projects });
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
